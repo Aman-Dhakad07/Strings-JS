3df593a552f0f87e9953f59b9b9018398180fce9
Aman-Dhakad07 <dhakadaman035@gmail.com>
2025-08-07 09:57:13 +0530

    Add files via upload
    Created strings  in javaScript ,and used its Application

---
+++ index.js
@@ -0,0 +1,71 @@
+
+// ++++++++++++++++++++++STRINGS+++++++++++++++++++++++++++++++++
+
+
+// const name="Hitesh";
+// const repoCount=50;
+
+// console.log(name+ repoCount+" value");
+
+// console.log(`Hello my name is $(name) and my repo count is
+//      $(repoCount)`);
+
+
+//      const gameName = new String('HiteshHC');
+
+
+//      console.log(gameName[0]);
+//           console.log(gameName.__proto__);
+
+//           console.log(gameName.charAt(3));
+//           console.log(gameName.indexOf('t'));
+
+//           const newString = gameName.substring(0,4);
+//           console.log(newString);
+
+//           const anotherString = gameName.slice(-8,4);
+//           console.log(anotherString);
+
+//           const url= "https://hitesh.com/hitesh%20choudhary"
+
+//           console.log(url.replace('%20','-'));
+
+
+
+
+// const score=500;
+
+// console.log(score);
+
+// const balance= new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber=1123.45654;
+// console.log(otherNumber.toPrecision(5));
+
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
+//   +++++++++++++++++++++++++++MATH+++++++++++++++++++++++++++++
+
+
+
+
+
+
+console.log.apply(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(2,4));
+console.log(Math.floor(4.9));
+
